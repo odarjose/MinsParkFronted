bc4d2e9ed4b6d9af8624119312c4aa2e4c5da570
Jose Odar <odarjimenezj@gmail.com>
2025-01-29 23:35:29 -0500

    Arreglando rutas de imagenes

--- src/components/specific/Header.tsx
+++ src/components/specific/Header.tsx
@@ -5,7 +5,7 @@ export function Header() {
     <nav className="shadow-transparent">
       <span className="self-center font-sans whitespace-nowrap  text-texto font-bold  dark:text-white">
         <img
-          src="src/assets/images/logo.png"
+          src="public/logo.png"
           alt="logo"
           className="w-32 translate-x-10 translate-y-2 object-contain bg-transparent"
         />
