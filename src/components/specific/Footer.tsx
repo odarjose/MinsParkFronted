0a395b45d1104b96a0273ab39b80206572617aaa
Jose Odar <odarjimenezj@gmail.com>
2025-02-02 00:25:02 -0500

    Home

---
+++ src/components/specific/Footer.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+export function Footer() {
+    return (
+        <footer className="bg-white mt-12">
+        <div className="max-w-7xl mx-auto px-4 py-6 sm:px-6 lg:px-8">
+          <p className="text-center text-gray-500">
+            Elaborado con ❤️ para jóvenes matemáticos
+          </p>
+        </div>
+      </footer>
+    );
+}
